52a4a8bc395a29a2f40981f5d1fd1067f2646b63
Kallio Mikko <mikko.kallio@elisa.fi>
2020-07-02 01:54:06 +0300

    Update tests

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -20,62 +20,63 @@ const noLikesBlog = {
   url: "www.example.com"
 }
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  await Blog.insertMany(helper.initialBlogs)
-})
-
-test('a valid blog can be added', async () => {
-  await api
-    .post('/api/blogs')
-    .send(testBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAfter = await helper.blogsInDb()
-  expect(blogsAfter).toHaveLength(helper.initialBlogs.length + 1)
-
-  const titles = blogsAfter.map(blog => blog.title)
-
-  expect(titles).toContain(
-    'My First Blog'
-  )
-})
-
-test('blog added without likes gets 0 likes', async () => {
-  await api
-    .post('/api/blogs')
-    .send(noLikesBlog)
-
-  const blogsAfter = await helper.blogsInDb()
-  expect(blogsAfter).toHaveLength(helper.initialBlogs.length + 1)
-
-  const lastBlog = blogsAfter.filter(blog => blog.title === 'My Unliked Blog')[0]
-
-  expect(lastBlog.likes).toBe(0)
-})
-
-test('blog without title or url is not added', async () => {
-  const newBlog = {
-    url: "www.example.com"
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsAfter = await helper.blogsInDb()
-
-  expect(blogsAfter).toHaveLength(helper.initialBlogs.length)
-})
-
-test('blog has an id', async () => {
+test('a blog has an id', async () => {
   const allBlogs = await helper.blogsInDb()
 
   expect(allBlogs[0].id).toBeDefined()
 })
 
+describe('adding a new blog', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+  })
+  
+  test('succeeds with 200 if valid', async () => {
+    await api
+      .post('/api/blogs')
+      .send(testBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAfter = await helper.blogsInDb()
+    expect(blogsAfter).toHaveLength(helper.initialBlogs.length + 1)
+  
+    const titles = blogsAfter.map(blog => blog.title)
+  
+    expect(titles).toContain(
+      'My First Blog'
+    )
+  })
+  
+  test('gets 0 likes if has no likes', async () => {
+    await api
+      .post('/api/blogs')
+      .send(noLikesBlog)
+  
+    const blogsAfter = await helper.blogsInDb()
+    expect(blogsAfter).toHaveLength(helper.initialBlogs.length + 1)
+  
+    const lastBlog = blogsAfter.filter(blog => blog.title === 'My Unliked Blog')[0]
+  
+    expect(lastBlog.likes).toBe(0)
+  })
+  
+  test('fails with 400 if no title or url', async () => {
+    const newBlog = {
+      url: "www.example.com"
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  
+    const blogsAfter = await helper.blogsInDb()
+  
+    expect(blogsAfter).toHaveLength(helper.initialBlogs.length)
+  })  
+})
 
 afterAll(() => {
   mongoose.connection.close()
