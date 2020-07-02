d09d683ec81a1f5505a94f01067303d7c2fa1bd9
Kallio Mikko <mikko.kallio@elisa.fi>
2020-07-02 15:41:12 +0300

    Add token auth

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,13 +1,34 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find()
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
+
   response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+  //const user = await User.findById(body.userId)
 
   if (!body.title || !body.url) {
     response.status(400).send({ error: 'blog must contain both title and url' })
@@ -16,10 +37,14 @@ blogsRouter.post('/', async (request, response) => {
       title: body.title,
       author: body.author,
       url: body.url,
-      likes: body.likes || 0
+      likes: body.likes || 0,
+      user: user._id
     })
-  
+
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.json(savedBlog.toJSON())
   }
 })
@@ -30,7 +55,7 @@ blogsRouter.delete('/:id', async (request, response) => {
 })
 
 blogsRouter.put('/:id', async (request, response) => {
-  const upd = await Blog.findByIdAndUpdate(request.params.id, {'likes': request.body.likes})
+  const upd = await Blog.findByIdAndUpdate(request.params.id, { 'likes': request.body.likes })
   response.json(upd)
 })
 
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const helper = require('./test_helper')
 const app = require('../app')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 const Blog = require('../models/blog')
 
 const api = supertest(app)
-
 
 const testBlog = {
   title: "My First Blog",
@@ -31,51 +32,105 @@ describe('adding a new blog', () => {
     await Blog.deleteMany({})
     await Blog.insertMany(helper.initialBlogs)
   })
-  
+
   test('succeeds with 200 if valid', async () => {
     await api
       .post('/api/blogs')
       .send(testBlog)
       .expect(200)
       .expect('Content-Type', /application\/json/)
-  
+
     const blogsAfter = await helper.blogsInDb()
     expect(blogsAfter).toHaveLength(helper.initialBlogs.length + 1)
-  
+
     const titles = blogsAfter.map(blog => blog.title)
-  
+
     expect(titles).toContain(
       'My First Blog'
     )
   })
-  
+
   test('gets 0 likes if has no likes', async () => {
     await api
       .post('/api/blogs')
       .send(noLikesBlog)
-  
+
     const blogsAfter = await helper.blogsInDb()
     expect(blogsAfter).toHaveLength(helper.initialBlogs.length + 1)
-  
+
     const lastBlog = blogsAfter.filter(blog => blog.title === 'My Unliked Blog')[0]
-  
+
     expect(lastBlog.likes).toBe(0)
   })
-  
+
   test('fails with 400 if no title or url', async () => {
     const newBlog = {
       url: "www.example.com"
     }
-  
+
     await api
       .post('/api/blogs')
       .send(newBlog)
       .expect(400)
-  
+
     const blogsAfter = await helper.blogsInDb()
-  
+
     expect(blogsAfter).toHaveLength(helper.initialBlogs.length)
-  })  
+  })
+})
+
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mkallio',
+      name: 'Mikko Koo',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('`username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+  })
 })
 
 afterAll(() => {
