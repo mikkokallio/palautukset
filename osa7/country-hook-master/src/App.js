9baea5e8d739a4230751ae3acce3cceb96c90cfd
Kallio Mikko <mikko.kallio@elisa.fi>
2020-07-23 05:12:26 +0300

    Fix country hook

--- osa7/country-hook-master/src/App.js
+++ osa7/country-hook-master/src/App.js
@@ -18,7 +18,21 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect()
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios(
+          `https://restcountries.eu/rest/v2/name/${name}?fullText=true`,
+        )
+
+        setCountry({ found: true, data: result.data[0] })
+      } catch (error) {
+        setCountry( null )
+      }
+    }
+
+    fetchData()
+  }, [name])
 
   return country
 }
@@ -40,8 +54,8 @@ const Country = ({ country }) => {
     <div>
       <h3>{country.data.name} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {country.data.population}</div>
+      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`} />
     </div>
   )
 }
